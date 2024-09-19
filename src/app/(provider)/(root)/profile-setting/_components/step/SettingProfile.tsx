95a572b8953477fb7d7301fde0bcef79607efc7f
parkchowon <70216263+parkchowon@users.noreply.github.com>
2024-09-19 19:12:47 +0900

    Feat: 프로필 세팅할때 사진 파일 불러오기

--- src/app/(provider)/(root)/profile-setting/_components/step/SettingProfile.tsx
+++ src/app/(provider)/(root)/profile-setting/_components/step/SettingProfile.tsx
@@ -3,6 +3,7 @@ import { getUser } from "@/apis/auth.api";
 import { useAuth } from "@/contexts/auth.context";
 import { useQuery } from "@tanstack/react-query";
 import Image from "next/image";
+import { ChangeEvent } from "react";
 import ProfileSettingContainer from "../ProfileSettingContainer";
 
 function SettingProfile() {
@@ -17,6 +18,17 @@ function SettingProfile() {
     },
     enabled: isInitialized,
   });
+
+  const handleImageChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const file = e.target.files?.[0];
+    if (file) {
+      console.log(file);
+    }
+  };
+
+  const handleProfileClick = () => {
+    document.getElementById("file-input")?.click();
+  };
 
   if (isPending || !userData) {
     return <p>loading...</p>;
@@ -35,11 +47,23 @@ function SettingProfile() {
             alt="profile"
             className="absolute rounded-full"
           />
-          <button className="absolute bottom-0 right-0 w-12 h-12 rounded-full bg-black opacity-25" />
+          {/** 컴퓨터에서 사진 받는 input */}
+          <input
+            type="file"
+            accept="image/*"
+            className="hidden"
+            id="file-input"
+            onChange={handleImageChange}
+          />
+          {/** 실제 사진을 고르기 위해 클릭하는 버튼 */}
+          <button
+            onClick={handleProfileClick}
+            className="absolute bottom-0 right-0 w-12 h-12 rounded-full bg-black opacity-25"
+          />
         </div>
         <input
           type="text"
-          className="w-[356px] py-3 px-[18px] border-b-[1px] font-medium"
+          className="w-[356px] py-3 px-[18px] border-b-[1px] font-medium outline-none"
           defaultValue={userData.nickname}
         />
       </div>
