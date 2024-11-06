686d7243cac7366a30ec31ae9a4a5677892e0838
parkchowon <70216263+parkchowon@users.noreply.github.com>
2024-11-07 01:04:10 +0900

    Chore: todo 주석 달기

--- src/app/(provider)/(root)/notifications/_components/FollowItem.tsx
+++ src/app/(provider)/(root)/notifications/_components/FollowItem.tsx
@@ -20,6 +20,7 @@ function FollowItem({ notification }: { notification: NotiType }) {
         <span className="font-bold">{notification.reacted_user_nickname}</span>{" "}
         님이 회원님을 팔로우하기 시작했습니다.
       </p>
+      {/* TODO: 팔로우 hook 가지고 와서 버튼 누르면 팔로우랑 언팔로우 되게 */}
       <button className="text-sm font-semibold px-9 py-2.5 ml-auto bg-gray-50 rounded-[10px]">
         {isPending ? "" : data ? "팔로잉" : "팔로우"}
       </button>
