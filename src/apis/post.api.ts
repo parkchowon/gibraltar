bab5412cbb4ef2550120f1cfb10fcd2c6156b906
parkchowon <70216263+parkchowon@users.noreply.github.com>
2024-11-29 19:08:26 +0900

    Add: axios 기본 url 설정해주기

--- src/apis/post.api.ts
+++ src/apis/post.api.ts
@@ -7,7 +7,7 @@ import {
   PostType,
   RepostFnType,
 } from "@/types/home.type";
-import axios from "axios";
+import apiClient from "./apiClient.api";
 
 export const createPost = async (post: CreatePostType, tags?: TagRow[]) => {
   let postMediaURLs = null;
@@ -22,16 +22,20 @@ export const createPost = async (post: CreatePostType, tags?: TagRow[]) => {
       formData.append("user_id", post.user_id);
 
       // 이미지 업로드 요청
-      const uploadResponse = await axios.post("/api/storage/post", formData, {
-        headers: {
-          "Content-Type": "multipart/form-data",
-        },
-      });
+      const uploadResponse = await apiClient.post(
+        "/api/storage/post",
+        formData,
+        {
+          headers: {
+            "Content-Type": "multipart/form-data",
+          },
+        }
+      );
 
       postMediaURLs = uploadResponse.data.urls; // 서버에서 반환된 URL 배열
     }
     /** posts 테이블에 post 저장 */
-    const data = await axios.post("/api/post", {
+    const data = await apiClient.post("/api/post", {
       content: post.content,
       images: postMediaURLs || null,
       user_id: post.user_id,
@@ -44,7 +48,7 @@ export const createPost = async (post: CreatePostType, tags?: TagRow[]) => {
     if (!!post.parent_user_id && post.user_id !== post.parent_user_id) {
       const type = post.quoted_post_id ? "quote" : "comment";
 
-      const { data: notiData } = await axios.post("/api/notification", {
+      const { data: notiData } = await apiClient.post("/api/notification", {
         reacted_user_id: post.user_id,
         type: type,
         user_id: post.parent_user_id,
@@ -60,7 +64,7 @@ export const createPost = async (post: CreatePostType, tags?: TagRow[]) => {
 /**  user가 팔로하고 있는 모든 유저의 게시글 불러오기 */
 export const getPost = async (userId: string | null, cursor: string | null) => {
   try {
-    const response = await axios.get<PostsType>(
+    const response = await apiClient.get<PostsType>(
       `api/post/following?user_id=${userId}&cursor=${cursor}`
     );
     return response.data;
@@ -73,7 +77,7 @@ export const getPost = async (userId: string | null, cursor: string | null) => {
 /** user의 포스팅과 리포스트 불러오기 */
 export const getUserPost = async (userId: string, cursor: string | null) => {
   try {
-    const response = await axios.get<PostsType>(
+    const response = await apiClient.get<PostsType>(
       `api/post/user/${userId}?cursor=${cursor}`
     );
     return response.data;
@@ -86,7 +90,7 @@ export const getUserPost = async (userId: string, cursor: string | null) => {
 // user의 media만 불러오기
 export const getUserMedia = async (userId: string, page: number) => {
   try {
-    const response = await axios.get<PostsType>(
+    const response = await apiClient.get<PostsType>(
       `api/post/user/${userId}/media?page=${page}`
     );
     return response.data;
@@ -99,7 +103,7 @@ export const getUserMedia = async (userId: string, page: number) => {
 // user의 bookmark한 것 불러오기
 export const getUserBookmark = async (userId: string, page: number) => {
   try {
-    const response = await axios.get<PostsType>(
+    const response = await apiClient.get<PostsType>(
       `api/post/user/${userId}/like?page=${page}`
     );
     return response.data;
@@ -114,7 +118,7 @@ export const fetchPostDetail = async (
   postId: string
 ): Promise<PostType | undefined> => {
   try {
-    const response = await axios.get(`/api/post/${postId}`);
+    const response = await apiClient.get(`/api/post/${postId}`);
     return response.data;
   } catch (error) {
     console.error(error);
@@ -124,7 +128,7 @@ export const fetchPostDetail = async (
 // 하나의 post 삭제하기
 export const deletePost = async (postId: string, userId: string) => {
   try {
-    await axios.delete(`/api/post/${postId}`, {
+    await apiClient.delete(`/api/post/${postId}`, {
       params: { userId: userId },
     });
   } catch (error) {
@@ -152,13 +156,13 @@ export const insertRepost = async (
   is_quoted?: boolean
 ) => {
   try {
-    const response = await axios.post(
+    const response = await apiClient.post(
       `api/post/${postId}/repost?user_id=${userId}&is_quoted=${is_quoted}`
     );
 
     if (response.status === 200 && !is_quoted) {
       // 인용은 post 생성 로직에서 알림 처리해서 인용이 아닌 repost만
-      await axios.post("api/notification", {
+      await apiClient.post("api/notification", {
         reacted_user_id: userId,
         type: "repost",
         user_id: postUserId,
@@ -177,12 +181,12 @@ export const deleteRepost = async ({
   postUserId,
 }: RepostFnType) => {
   try {
-    const response = await axios.delete(
+    const response = await apiClient.delete(
       `api/post/${postId}/repost?user_id=${userId}`
     );
 
     if (response.status === 200) {
-      await axios.delete(
+      await apiClient.delete(
         `api/notification?related_post_id=${postId}&reacted_user_id=${userId}&user_id=${postUserId}&type=repost`
       );
     }
@@ -201,11 +205,11 @@ export const clickLike = async ({
   try {
     if (state) {
       //like 반응
-      const response = await axios.post(
+      const response = await apiClient.post(
         `api/post/${postId}/like?user_id=${userId}`
       );
       if (response.status === 200) {
-        await axios.post("api/notification", {
+        await apiClient.post("api/notification", {
           reacted_user_id: userId,
           type: "like",
           user_id: postUserId,
@@ -215,11 +219,11 @@ export const clickLike = async ({
       }
     } else {
       // like 취소
-      const response = await axios.delete(
+      const response = await apiClient.delete(
         `api/post/${postId}/like?user_id=${userId}`
       );
       if (response.status === 200) {
-        await axios.delete(
+        await apiClient.delete(
           `api/notification?related_post_id=${postId}&reacted_user_id=${userId}&user_id=${postUserId}&type=like`
         );
       }
